c2996b8c2d8b51bd442bdb8aa2dafbce4296fd5c
astefanczyk <astefanczyk@student.agh.edu.pl>
2022-08-31 15:56:26 +0200

    v12

--- IR_detection/main.py
+++ IR_detection/main.py
@@ -5,7 +5,6 @@ import collections
 import pandas as pd
 collections.Callable = collections.abc.Callable
 import tensorflow as tf
-from tensorflow.python.keras.optimizers import adam_v2
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Flatten
 from tensorflow.python.keras.layers.convolutional import Conv2D, MaxPooling2D
@@ -32,13 +31,13 @@ y_train = training_targets[:,idx].T
 
 idx = np.random.permutation(len(testing_data))
 
-x_test = testing_data[idx,:,:,:]
-y_test = testing_tagrets[:,idx].T
+x_test = testing_data
+y_test = testing_tagrets.T
 
 def cnn_model():
 
     filters = 12
-    kernel = (3,3)
+    kernel = (4,4)
     l1_rate = 1e-3
 
     model=Sequential()
@@ -52,18 +51,11 @@ def cnn_model():
                      activation='relu', kernel_regularizer=tf.keras.regularizers.l1(l1_rate)))
     model.add(MaxPooling2D(pool_size=(2,2), padding='same'))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(8, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
-    opt = adam_v2.Adam(
-            learning_rate=1e-2,
-            beta_1=0.9,
-            beta_2=0.999,
-            epsilon=1e-07,
-            amsgrad=False,
-            name='Adam'
-        )
     model.compile(loss='BinaryCrossentropy', optimizer='Adam', metrics=['accuracy'])
     model.summary()
     return model
@@ -71,7 +63,7 @@ def cnn_model():
 model = cnn_model() 
 
 batch_size = 20
-epochs = 100
+epochs = 50
 try:
     history = model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs)
 finally:
