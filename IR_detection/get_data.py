f5e325f8d3ad8009330a8d664e11061f4dc24223
astefanczyk <astefanczyk@student.agh.edu.pl>
2022-08-31 10:00:01 +0200

    v2

--- IR_detection/get_data.py
+++ IR_detection/get_data.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 import json
 
-path = 'E:\\python_codes\\rumcajs\\data\\video_thermal_test\\index.json'
+path = 'E:\\IR_detection\\IR_detection\\IR_detection\\data\\test\\index.json'
 with open(path) as json_data:
     data = json.load(json_data)
 df = pd.DataFrame(data['frames'])
 df = df[['videoMetadata', 'annotations']]
-print(df.loc[[1]]['annotations'][1][0]['labels'])
+print(df.loc[[0]]['annotations'][0][0]['labels'])
+print(df.loc[[1]]['videoMetadata'][1])
